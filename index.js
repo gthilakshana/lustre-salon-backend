3e3e69f0f78a199b0de4a836c332d5b31da81ec1
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-26 10:19:12 -0700

    first commit 😊

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+app.get('/',
+    (req, res)=>{
+        console.log(req.body)
+        console.log("Hello World")
+ 
+        res.json({
+            message: "Hello "+ prefix + " " + req.body.name
+        })
+    }
+)
+
+app.post('/',
+    (req , res)=>{
+        console.log("Hello World")
+    }
+)
+
+app.delete('/',
+    (req , res)=>{
+        console.log("Delete request")
+    }
+)
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
+    console.log("Server is running on port 5000")
+})
