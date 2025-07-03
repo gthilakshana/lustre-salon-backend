c01a9dbacf361cfb8a73411dd63eb05cc941572d
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-07-03 09:33:20 -0700

    model created ,mongoose install and nodemon  added  👍

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import Student from "./models/student.js";
 
 const app = express()
 
 // Middleware
 app.use(express.json())
 // Middleware
+
+
+const connectionString =  "mongodb+srv://admin:123@cluster0.ya0uqag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(connectionString).then(
+    () => {
+    console.log("Connected to MongoDB")
+  }
+).catch(
+    ()=>{
+        console.log("Database connection failed")
+    }
+)
 
 app.get('/',
     (req, res)=>{
