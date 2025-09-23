887de785ec30ba64b5d473980ee0e45ded83481e
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-09-23 06:14:26 -0700

    Router Update

--- routes/stripeRouter.js
+++ routes/stripeRouter.js
@@ -72,9 +72,8 @@ router.post("/create-checkout-session", authMiddleware, async (req, res) => {
       payment_method_types: ["card"],
       line_items,
       mode: "payment",
-     success_url: `https://lustre-salon.vercel.app/success?session_id={CHECKOUT_SESSION_ID}`,
-     cancel_url: `https://lustre-salon.vercel.app/`,
-
+      success_url: `${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
       metadata: { appointmentIds: JSON.stringify(savedIds), paymentOption },
     });
 
